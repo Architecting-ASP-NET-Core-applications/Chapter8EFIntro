6e0c2639cb797e4dc64e003e018b1c037b16be32
BitawareUnleashed <dario.benevento@gmail.com>
2024-04-29 20:57:36 +0200

    Initial commit

---
+++ Chapter8EFIntro/Chapter8EFIntro/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Chapter8EFIntro.Models;
+
+public class DataContext : DbContext
+{
+    public DbSet<Book> Books { get; set; }
+    protected override void OnConfiguring
+        (DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite
+                ("Data Source=blazorwithefcore.db");
+    public void InitializeDatabase()
+        => Database.EnsureCreated();
+}
+
